82dc8ecce31e90dc50d519cebd072999029d6b95
Bikochii <anton.pelezki@gmail.com>
2022-11-09 11:05:46 +0100

    3-x Angepassst

--- p5-matter-examples-master/3-x/sketch.js
+++ p5-matter-examples-master/3-x/sketch.js
@@ -4,7 +4,7 @@ let ground;
 
 
 function setup() {
-  const canvas = createCanvas(800, 600);
+  const canvas = createCanvas(1200, 700);
 
   // create an engine
   let engine = Matter.Engine.create();
@@ -13,27 +13,27 @@ function setup() {
   // add blocks
   const group = Matter.Body.nextGroup(true);
   rect1 = new Block(world,
-    { x: 400, y: 200, w: 400, h: 40, color: 'white' },
+    { x: 400, y: 200, w: 400, h: 40, color: 'yellow' },
     {collisionFilter: {group: group}, angle: PI/10*4}
   );
   rect2 = new Block(world,
-    { x: 400, y: 200, w: 400, h: 40, color: 'white' },
+    { x: 400, y: 200, w: 400, h: 40, color: 'yellow' },
     {collisionFilter: {group: group}, angle: PI-PI/10*4}
   );
 
   // revolute
-  rect2.constrainTo(rect1, { length: 0, stiffness: 1 });
+  rect2.constrainTo(rect1, { length: 2, stiffness: 2 });
 
   // rubberband
   rect1.constrainTo(rect2, {
-    pointA: {x: 25, y: 80},
-    pointB: {x: -25, y: 80},
-    length: 70,
+    pointA: {x: 5, y: 60},
+    pointB: {x: -5, y: 40},
+    length: 60,
     stiffness: 0.5
   });
 
   // ground
-  ground = new Block(world, {x:400, y: height, w: 810, h: 100, color: 'white'}, {isStatic: true});
+  ground = new Block(world, {x:400, y: height, w: 810, h: 100, color: 'blue'}, {isStatic: true});
 
   // setup mouse
   mouse = new Mouse(engine, canvas);
